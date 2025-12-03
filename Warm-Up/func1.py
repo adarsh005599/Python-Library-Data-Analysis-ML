681ceabd0bdcdde92459350798f9261267186309
adarsh005599 <happysingh005599@gmail.com>
2025-12-03 18:19:22 +0530

    sep folders added

---
+++ Warm-Up/func1.py
@@ -0,0 +1,13 @@
+# squre of num
+def squre_of_num(num):
+    return(num **3)
+    
+result=squre_of_num(6)
+print(result)
+
+#two value sum
+def Add(num1,num2):
+    return num1+num2
+
+result=Add(2,3)
+print(result)
